fix double softmax

model.py
@@ -113,16 +113,14 @@ class GPT(nn.Module):
         self.transformer_blocks = nn.Sequential(*[TransformerBlock(d_in, num_heads, block_size) for _ in range(num_blocks)])
         self.decoder = nn.Linear(d_in, dict_size)
 
-    def forward(self, x: Tensor):
+    def forward(self, x: Tensor, targets: Tensor | None = None) -> Tensor | tuple[Tensor, Tensor]:
         x1 = self.embedding(x)
         x2 = GPT._positional_encoding(x1.shape[1], x1.shape[2]).to(x.device) + x1
         x3 = self.transformer_blocks(x2)
-        x4 = self.decoder(x3)
-        return F.softmax(x4, dim=2)
-    
-    def forward_train(self, x: Tensor, targets: Tensor):
-        logits = self.forward(x)
-        return logits, F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1)) #?ignore_index ?this is for padding I believe
+        logits = self.decoder(x3)
+        if targets is not None:
+            return logits, F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1)) #?ignore_index ?this is for padding I believe
+        return logits
 
     @staticmethod
     def _positional_encoding(l: int, dim: int) -> Tensor:
@@ -137,6 +135,7 @@ class GPT(nn.Module):
         a = [start_char]
         for _ in range(max_len-1):
             so_far_pred = torch.stack(a).unsqueeze(0)
-            char = self.forward(so_far_pred)[0][0].multinomial(1)
+            logits: Tensor = self.forward(so_far_pred)
+            char = logits.softmax(dim=2)[0][0].multinomial(1)
             a.append(char[0])
         return [i.item() for i in a]

train.py
@@ -1,6 +1,5 @@
 import torch
 from model import GPT
-from typing import Callable
 
 
 def infinite():
@@ -9,12 +8,12 @@ def infinite():
     yield i
     i += 1
 
-batch_size = 64
-d_in = 32 + 16
-block_size = 8
+batch_size = 256
+d_in = 128
+block_size = 64
 train_pct = 0.8
-num_block = 4
-num_heads = 2
+num_block = 6
+num_heads = 4
 
 if __name__ == "__main__":
   with open('input.txt', 'r', encoding='utf-8') as f:
@@ -31,33 +30,32 @@ if __name__ == "__main__":
 
   train = data[:int(train_pct*len(data))].to(device)
 
-  gpt = GPT(
+  model = GPT(
     d_in=d_in,
     dict_size=len(chars),
     num_heads=num_heads,
     num_blocks=num_block,
     block_size=block_size,
-    device=device,
   ).to(device)
 
-  optimizer = torch.optim.Adam(gpt.parameters(), lr=8e-4)
+  optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
   batch_len = block_size * batch_size
   print(f"should do {len(train) // batch_len} batches per epoch")
 
   for epoch in infinite():
     for batch_idx in range(len(train) // batch_len):
-      gpt.train()
+      model.train()
       batch_x = train[batch_idx * batch_len : (batch_idx+1) * batch_len].reshape(batch_size, block_size)
       batch_y = torch.roll(batch_x, -1, dims=1)
       optimizer.zero_grad()
-      logits, loss = gpt.forward_train(batch_x, batch_y)
+      logits, loss = model.forward(batch_x, batch_y)
       loss.backward()
       optimizer.step()
       print(f'epoch {epoch} batch {batch_idx} loss {loss.item()}')
-      print(f'<>{decode(batch_x[0].cpu().numpy())}<>\n<>{decode(batch_y[0].cpu().numpy())}<>\n<>{decode(torch.argmax(logits[0], dim=1).cpu().numpy())}<>\n\n')
-      break
+      if batch_idx % 100 == 0:
+        print(f'<>{decode(batch_x[0].cpu().numpy())}<>\n<>{decode(batch_y[0].cpu().numpy())}<>\n<>{decode(torch.argmax(logits[0], dim=1).cpu().numpy())}<>\n\n')
 
-    # gpt.eval()
-    # sentence = gpt.infer_random(block_size // 3)
-    # print(f'epoch {epoch}')
-    # print(f"<>{decode(sentence)}<>\n\n")
+    model.eval()
+    sentence = model.infer_random(block_size // 3)
+    print(f'epoch {epoch}')
+    print(f"<>{decode(sentence)}<>\n\n")