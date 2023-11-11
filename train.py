ecaadd1b154ad9e15288a55f7f969f8521282a01
Oli <o.clivegriffin@gmail.com>
2023-11-11 23:56:24 +1300

    tokenizer, wip

--- train.py
+++ train.py
@@ -5,6 +5,9 @@ from model import GPT
 import itertools
 from typing import Iterable
 import os
+from torch.utils.data import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from tokenizers import ByteLevelBPETokenizer
 
 
 class Corpus:
@@ -12,10 +15,15 @@ class Corpus:
         self.text = text
         self.chars = sorted(list(set(text)))
         self.char_to_idx =  { ch: i for i, ch in enumerate(self.chars) }
-        self.dict_size = len(self.chars)
 
+        self.tokenizer = ByteLevelBPETokenizer()
+        self.tokenizer.train(files="input.txt", vocab_size=512)
+        self.dict_size = 512
+        
     def encode(self, s: str):
-        return [self.char_to_idx[ch] for ch in s]
+        encoding = self.tokenizer.encode(s)
+        vocab = self.tokenizer.get_vocab()
+        return [vocab[tok] for tok in encoding.tokens]
 
     def decode(self, xs: Iterable[int]):
         return ''.join([self.chars[x] for x in xs])
@@ -31,7 +39,7 @@ class TextDataset(Dataset[torch.Tensor]):
     def __len__(self):
         return len(self.sequences)
 
-    def __getitem__(self, idx) -> (torch.Tensor, torch.Tensor):
+    def __getitem__(self, idx) -> tuple[torch.Tensor, torch.Tensor]:
         sequence = self.sequences[idx]
         return sequence[:-1], sequence[1:]
 
@@ -41,27 +49,33 @@ def create_data_loaders(text_enc: list[int], block_size: int, train_pct: float, 
 
     train_size = int(train_pct * len(whole_dataset))
     val_size = len(whole_dataset) - train_size
-    train_data, val_data = random_split(whole_dataset, [train_size, val_size])
+    train_dataset, val_dataset = random_split(whole_dataset, [train_size, val_size])
 
-    train_loader = DataLoader(train_data, batch_size, shuffle=True)
-    val_loader = DataLoader(val_data, batch_size, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size, sampler=DistributedSampler(train_dataset))
+    val_loader = DataLoader(val_dataset, batch_size, sampler=DistributedSampler(val_dataset))
 
     return train_loader, val_loader
 
 
+local_rank = int(os.getenv("LOCAL_RANK"))
+torch.cuda.set_device(local_rank)
+
+torch.distributed.init_process_group(backend="nccl")
+
 if __name__ == "__main__":
 
     file_path = 'input.txt'
-    block_size = 128
+    block_size = 512
     train_pct = 0.9
-    batch_size = 32
-    d_in = 64 * 6
+    batch_size = 128
     num_block = 6
-    num_heads = 6
-    log_wandb = 'WANDB_KEY' in os.environ
+    num_heads = 4
+    d_in = 64 * num_heads
     dropout = 0.3
 
-    device = torch.device('mps')
+    log_wandb = 'WANDB_KEY' in os.environ
+
+    device = torch.device('cuda')
 
     with open(file_path, 'r', encoding='utf-8') as f:
         text = f.read().lower()
@@ -69,6 +83,7 @@ if __name__ == "__main__":
 
     train_loader, val_loader = create_data_loaders(corpus.encode(corpus.text), block_size, train_pct, batch_size)
     model = GPT(d_in, corpus.dict_size, num_heads, num_block, block_size, dropout).to(device)
+    model = DDP(model)
     optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
 
     if log_wandb:
@@ -83,6 +98,8 @@ if __name__ == "__main__":
                 'dropout': dropout,
             },
         )
+    
+    lrs = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=50)
 
     for epoch in itertools.count():
         if log_wandb:
@@ -96,6 +113,9 @@ if __name__ == "__main__":
             logits, loss = model.forward(batch_x, batch_y)
             loss.backward()
             optimizer.step()
+            lrs.step()
+
+            # if batch_idx % 100 == 99:
             print(f'epoch {epoch} batch {batch_idx} loss {loss.item()}')
 
             if log_wandb and batch_idx % 10 == 0:
